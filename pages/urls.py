99bbc221b4aa38e45e2481c9065324a6760f1a99
Sam-MJ <tf2redspark@gmail.com>
2024-06-04 22:36:57 +0100

    added index and about html templates

--- pages/urls.py
+++ pages/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+app_name = "pages"
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("about", views.about, name="about"),
+    path("products", views.products, name="products"),
+    path("contact", views.contact, name="contact"),
+    path("articles", views.articles, name="articles"),
+]
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, "pages/index.html")
+
+
+def about(request):
+    title = "About"
+    context = {"title": title}
+    return render(request, "pages/about.html", context)
+
+
+def products():
+    return
+
+
+def contact():
+    return
+
+
+def articles():
+    return
