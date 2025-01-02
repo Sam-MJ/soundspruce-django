5b441cfaa40b038d003c5e987e9d77ea9c7ec8e5
Sam-MJ <tf2redspark@gmail.com>
2025-01-02 19:18:55 +0000

    added enquires to admin panel

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .models import Enquiry
+
+
+@admin.register(Enquiry)
+class MyUserAdmin(UserAdmin):
+    pass
