8d9fb9861885aa85356005cc0774fa91634f4f51
Sam-MJ <tf2redspark@gmail.com>
2025-01-17 14:47:41 +0000

    added animated counter to home page

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,10 +3,65 @@
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+// Navbar, currently active page
 document.querySelectorAll(".nav-link").forEach((link) => {
     if (link.href === window.location.href) {
         link.classList.add("active");
     }
+});
+
+// Counter animation, start at text content number, go up to data-val in random increments after a random amount of time
+let animatedCounter = (function() {
+    //closure to ensure runCounter is only called once.
+    let running = false;
+    return function runCounter(element) {
+        if (running) {
+            return
+        }
+        running = true
+
+        let startValue = Number(element.textContent);
+        let maxValue = parseInt(element.getAttribute("data-val"))
+        let delay = getRandomBetween(4000, 30000)
+        console.log(delay)
+
+        // how to change set interval to a new delay each time it runs?
+        let totalCounter = setInterval(() => {
+            // if the final max total has been reached exit
+            if (startValue > maxValue) {
+                clearInterval(totalCounter)
+            }
+
+            // add an ammount between 10 and 200 to the counter at random duration between 4 and 30 seconds.
+            let addValue = getRandomBetween(10, 200)
+            let endValue = startValue + addValue
+            let duration = Math.floor(interval / addValue)
+
+            let counter = setInterval(() => {
+                startValue += 1
+                element.textContent = startValue;
+                if (startValue > endValue){
+                    clearInterval(counter)
+                }
+            }, duration);
+        }, delay);
+    }
+})();
+
+let valueDisplays = document.querySelectorAll(".spinner-number");
+let interval = 3000
+
+function getRandomBetween(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+valueDisplays.forEach((valueDisplay) => {
+    const observer = new IntersectionObserver(entries => {
+        if (entries[0].target.checkVisibility()) {
+            animatedCounter(valueDisplay)
+            console.log("visible!")
+        }
+    });
+    observer.observe(valueDisplay)
 });
