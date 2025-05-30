afc47a8aed44985f12b35ffc94014c74421e2cb9
Sam-MJ <tf2redspark@gmail.com>
2025-05-30 18:41:56 +0100

    updated settings to load specific .env files for dev/production

--- soundspruce/settings.py
+++ soundspruce/settings.py
@@ -17,7 +17,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
+load_dotenv(dotenv_path=Path(".env.development"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
